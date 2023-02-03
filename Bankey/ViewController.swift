ef93fcc46b9d109f0410e12d23455f7978220b4d
Silva Kirsimae <silvakirsimae@gmail.com>
2023-02-03 10:56:40 +0100

    Initial Commit

---
+++ Bankey/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bankey
+//
+//  Created by Silva Kirsimae on 03/02/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
